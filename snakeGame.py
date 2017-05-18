e584999643ffeac98db51d08954325211472e69d
Shahidul Islam <shawon_math07@yahoo.com>
2017-05-18 11:19:25 +0600

    Initial commit

---
+++ snakeGame.py
@@ -0,0 +1,10 @@
+# pygame sound & graphics
+import pygame
+# sys -> System exit the program
+import sys
+# time sleep & message display
+import time
+# random for different coordinate
+import random
+
+pygame.init()
