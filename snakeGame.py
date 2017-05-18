bdf945417182d765359c06ce2cec3494ac8f570e
Shahidul Islam <shawon_math07@yahoo.com>
2017-05-18 11:40:31 +0600

    creating Game Screen & holding 2 sec

--- snakeGame.py
+++ snakeGame.py
@@ -1,5 +1,5 @@
 # pygame sound & graphics
-import pygame
+import pygame as p
 # sys -> System exit the program
 import sys
 # time sleep & message display
@@ -11,7 +11,7 @@ import random
 # print(pygame.init())
 
 # apply condition based on initialize value
-check_errors = pygame.init()
+check_errors = p.init()
 # (6, 0)
 if check_errors[1] > 0:
     print("(!) Had {0} initializing errors,"
@@ -19,3 +19,11 @@ if check_errors[1] > 0:
     sys.exit(-1)
 else:
     print("(+) Game initializing Succesfully")
+
+# Game Screen
+# changing window name
+p.display.set_caption("Snake Game")
+# gamescreen width,height initialize but function accept one parameter
+gamescreen = p.display.set_mode((720, 460))
+# hold the gamescreen
+time.sleep(2)
